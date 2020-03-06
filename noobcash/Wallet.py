1e0704148912c9a52b7be538a92af53aa93a8970
Groute <iAmGroute@outlook.com>
2020-03-06 21:55:08 +0200

    Update Wallet.py

--- noobcash/Wallet.py
+++ noobcash/Wallet.py
@@ -1,14 +1,35 @@
 
 class Wallet:
 
-    def __init__(self, node, pubkey):
-        self.node   = node
-        self.pubkey = pubkey
+    def __init__(self, nbc, keyFile):
+        self.nbc    = nbc
+        self.keyFile = keyFile
+        self.pubkey  = None
+        self.privkey = None
+        if not self.loadKeys():
+            self.generateKeys()
+
+    def loadKeys(self):
+        try:
+            with open(self.keyFile) as f:
+                # TODO: read pem data
+                f.read()
+                self.pubkey  = b'ABCDEF'
+                self.privkey = b'QWERTY'
+            return True
+        except FileNotFoundError:
+            return False
+
+    def generateKeys(self):
+        # TODO: generate random RSA keys
+        self.pubkey  = b'ABCDEF'
+        self.privkey = b'QWERTY'
+        with open(self.keyFile, 'w') as f:
+            # TODO: write pem data
+            f.write('PEM keys go here\n')
 
     def getBalance(self):
-        return self.node.blockchain.getBalance(self.pubkey)
+        return self.nbc.blockchain.getBalance(self.pubkey)
 
     def getUTXOs(self):
-        return self.node.blockchain.getUTXOs(self.pubkey)
-
-
+        return self.nbc.blockchain.getUTXOs(self.pubkey)
