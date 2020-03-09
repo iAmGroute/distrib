bd26020e1cff7b8abbf726227bbad4558c4cd405
Groute <iAmGroute@outlook.com>
2020-03-09 15:53:11 +0200

    Block: added field `myID`

--- noobcash/Block.py
+++ noobcash/Block.py
@@ -4,27 +4,32 @@ from Transaction import Transaction
 class Block:
 
     def __init__(self):
+        # Header
+        self.myID      = None
         self.nonce     = None
         self.thisHash  = None
         self.prevHash  = None
+        # Body
         self.timestamp = None
         self.txs       = None
 
     @staticmethod
     def fromJson(data):
         b = Block()
-        b.nonce     = bytes.fromhex(data[0])
-        b.thisHash  = bytes.fromhex(data[1])
-        b.prevHash  = bytes.fromhex(data[2])
-        b.timestamp = bytes.fromhex(data[3])
+        b.myID      = data[0]
+        b.nonce     = bytes.fromhex(data[1])
+        b.thisHash  = bytes.fromhex(data[2])
+        b.prevHash  = bytes.fromhex(data[3])
+        b.timestamp = bytes.fromhex(data[4])
         b.txs       = [
             Transaction.fromJson(tx)
-            for tx in data[4]
+            for tx in data[5]
         ]
         return b
 
     def toJson(self):
         data = [
+            self.myID,
             self.nonce.hex(),
             self.thisHash.hex(),
             self.prevHash.hex(),
--- noobcash/Miner.py
+++ noobcash/Miner.py
@@ -18,16 +18,15 @@ class Miner:
 
     def mineOneBlock(self):
         b = Block()
-        blockId = len(self.nbc.blockchain.blocks)
-        b.prevHash  = self.nbc.blockchain.blocks[blockId - 1].thisHash
+        b.myID      = len(self.nbc.blockchain.blocks)
+        b.prevHash  = self.nbc.blockchain.blocks[b.myID - 1].thisHash
         b.timestamp = (int(time.time()) & 0xFFFFFFFF).to_bytes(4, 'little')
         b.txs       = []
-        j = 0
         b.nonce     = b'....'
+        j = 0
         for i in range(random.randrange(1, 11) * (10 ** 7)):
             j += i if i % 7 else -5 * i
         b.thisHash  = (j & 0xFFFFFFFF).to_bytes(4, 'little')
         self.nbc.blockchain.addBlock(b)
-        print('Mined block', blockId)
-
+        print('Mined block', b.myID)
 
