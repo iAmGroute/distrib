ac8c69fa90d6b89d7727df8b3e6155dcc6efc163
Groute <iAmGroute@outlook.com>
2020-03-14 17:47:01 +0200

    webui Network: added 'syncing' status badge to neighbors

--- webui/src/pages/Network.svelte
+++ webui/src/pages/Network.svelte
@@ -18,11 +18,12 @@
             <th>Host</th>
             <th>Port</th>
             <th>Latest block</th>
+            <th>Connected</th>
             <th>Status</th>
           </tr>
         </thead>
         <tbody>
-          {#each state.content.status.neighbors as {peerName, lastBlockID, connected}, i}
+          {#each state.content.status.neighbors as {peerName, lastBlockID, connected, isSyncing}, i}
             <tr>
               <td>{i}</td>
               <td>{peerName[0]}</td>
@@ -31,6 +32,11 @@
               <td>
                 <span class={connected ? 'badge badge-success' : 'badge badge-danger'}>
                   {connected ? 'OK' : 'DISC'}
+                </span>
+              </td>
+              <td>
+                <span class={!isSyncing ? 'badge badge-success' : 'badge badge-warning'}>
+                  {!isSyncing ? 'OK' : 'Syncing'}
                 </span>
               </td>
             </tr>
