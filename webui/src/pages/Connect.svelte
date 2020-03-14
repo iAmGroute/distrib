d3019dbf8c72d70b05101ac68000687e0321992a
Groute <iAmGroute@outlook.com>
2020-03-14 17:47:26 +0200

    webui Connect: added quick connect shortcuts

--- webui/src/pages/Connect.svelte
+++ webui/src/pages/Connect.svelte
@@ -1,8 +1,8 @@
 <script>
   import { get } from '../api.js';
 
-  let canClick = true;
   let lines    = '';
+  let canClick = true;
   let btnText  = 'Connect';
   let btnClass = 'btn-primary';
 
@@ -36,6 +36,42 @@
       canClick = true;
     }
   }
+
+  const knownHosts = [
+    ['10.0.0.1', 5000],
+    ['10.0.1.0', 5000],
+    ['10.0.1.1', 5000],
+    ['10.0.1.2', 5000],
+    ['10.0.1.3', 5000]
+  ];
+  let qcStates = knownHosts.map(([host, port]) => ({
+    host,
+    port,
+    canClick: true,
+    btnText:  'Connect',
+    btnClass: 'btn-primary'
+  }));
+
+  console.log(qcStates);
+
+  async function handleClickQC(i) {
+    const s = qcStates[i];
+    if (!s.canClick) return;
+    s.canClick = false;
+    try {
+      s.btnText  = 'Connecting';
+      s.btnClass = 'btn-warning';
+      await get('connectToNeighbor', {host: s.host, port: s.port});
+      s.btnText  = 'Will connect';
+      s.btnClass = 'btn-success';
+    }
+    catch (error) {
+      s.btnText  = 'Connect failed';
+      s.btnClass = 'btn-danger';
+      s.canClick = true;
+    }
+    qcStates[i] = s;
+  }
 </script>
 
 <div class="col-lg-6">
@@ -48,6 +84,26 @@
     </div>
     <div class="card-footer">
       <button on:click={handleClick} class="btn {btnClass}">{btnText}</button>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-6">
+  <div class="card card-primary card-outline">
+    <div class="card-header">
+      <h5 class="card-title">Quick connect</h5>
+    </div>
+    <div class="card-body">
+      <!-- <ul class="list-group-flush"> -->
+      {#each qcStates as {host, port, canClick, btnText, btnClass}, i}
+        <!-- <li class="list-group-item text-center"> -->
+        <p>
+          <span class="mx-3">{host}:{port}</span>
+          <button on:click={() => handleClickQC(i)} class="btn {btnClass}">{btnText}</button>
+        </p>
+        <!-- </li> -->
+      {/each}
+      <!-- </ul> -->
     </div>
   </div>
 </div>
